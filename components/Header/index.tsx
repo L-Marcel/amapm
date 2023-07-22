c46b94c7b676ac56ff61a32522bafa0e203b3a11
l-marcel <lmgh1312@gmail.com>
2023-07-22 14:38:08 -0300

    style: adicionando responsividade na página inicial

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -10,18 +10,36 @@ interface HeaderRootProps {
 function HeaderRoot({ children }: HeaderRootProps) {
   return (
     <header className={style.container}>
-      <figure>
-        <Image 
-          alt="" 
-          width={175} 
-          height={175} 
-          src="/assets/logo.png"
-        />
-      </figure>
-      <nav>
-        <h1>AMAPM - Associação Cristã de Moradores e Amigos da Praia do Meio</h1>
-        <ul>{children}</ul>
-      </nav>
+      <div>
+        <figure>
+          <Image 
+            alt="" 
+            width={175} 
+            height={175} 
+            src="/assets/logo.png"
+          />
+        </figure>
+        <nav>
+          <h1>AMAPM - Associação Cristã de Moradores e Amigos da Praia do Meio</h1>
+          <ul>{children}</ul>
+        </nav>
+      </div>
+      <div>
+        <div>
+          <figure>
+            <Image 
+              alt="" 
+              width={100} 
+              height={100} 
+              src="/assets/logo.png"
+            />
+          </figure>
+          <h1>AMAPM - Associação Cristã de Moradores e Amigos da Praia do Meio</h1>
+        </div>
+        <nav>
+          <ul>{children}</ul>
+        </nav>
+      </div>
     </header>
   );
 };
