ef7ef43440e7de81a163b0b7c87e677116130c74
l-marcel <lmgh1312@gmail.com>
2023-07-23 15:42:57 -0300

    feat: reajustando fontes e se preparando para construir a página de notícias

--- app/noticias/page.tsx
+++ app/noticias/page.tsx
@@ -1,81 +1,16 @@
-import Link from "next/link";
+import { Post } from "@/components/Post";
+import style from "./index.module.scss";
 
 export default function News() {
   return (
-    <main>
-      <article id="art1">
-        <h3>Ata da reunião do mês de maio </h3>	
-        <h2>Dia 30/05/2023 por Ana Paula </h2>
-        <h4>logo de instagram e facebook</h4>
-        <p> Data: 30 de maio de 2023</p>
-        <p>Horário: 19:30 - 21:30</p>
-        <p>Local: Centro Social Nossa Senhora de Fátima na Praia do Meio</p>
-        <p>Presentes: Milton França(Presidente da AMAPM),Paulo Henrique (1oTesoureiro da
-        AMAPM), Nivaldo Targino(2oTesoureiro da AMAPM),Ana Paula (Departamento de
-        comunicações da AMAPM), Francisco Rodrigues(Diretor do Projeto Foco de Luz de
-        extensão Universitária), Ana Jart(Cooperativa Colina), Patrícia Karina(Cooperativa
-        Colina), Maria Dalva, Ivanilson (Balaio de Surf), Fernando( Presidente da Fecap/RN),
-        Francinete Venâncio, Tia Mundinha, Júlia e Marcos(assessores do Dep. Ubaldo
-        Fernandes), Luiz Antônio(do Centro Social de Brasilia Teimosa), Edna Maria, Prof.
-        Paulo Vitor, Jaqueline Venâncio, Emanoel, Raimunda Trajano, Oliver Santos(Rocas
-        Divulgações) e Charles(Rocas Divulgações)</p>
-        <p>Presentes: 22 pessoas</p>
-
-        <p>Apresentação do Rocas Divulgações</p>
-        <p>• Charles e Oliver fizeram uma apresentação sobre o trabalho realizado pela Rocas
-        Divulgações, por meio do Instagram {'"'}Rocas Divulgações{'"'}, que tem como objetivo
-        dar voz às comunidades, inicialmente focando nas Rocas, mas com a
-        possibilidade de expansão para outros bairros. Eles demonstraram disposição em
-        colaborar com a associação no que fosse possível.</p>
-        <p>Questão do novo Hospital Municipal de Natal</p>
-        <p>• Professor Milton França, presidente da associação, explicou que durante a 9a
-        conferência de Saúde, a associação participou da decisão sobre o local do novo
-        Hospital Municipal de Natal. Foi decidido que o local mais apropriado seria debaixo
-        da ponte, porém, o prefeito não aceitou essa decisão e realocou a obra para a
-        região de Pitimbu. O professor explicou que essa mudança não geraria custos
-        adicionais e que existe a possibilidade de a obra ser embargada devido à falta de
-        respeito à decisão pública. Além disso, propôs a alocação do novo Hospital
-        Estadual na região de Pitimbu, uma vez que o Hospital Estadual atende facilmente
-        as regiões mais distantes da capital. A proposta de ter o hospital municipal na
-        Redinha é mais acessível para o município.</p>
-        <p>Audiência sobre o terreno abandonado da Petrobrás</p>
-        <p>• Júlia, assessora do Deputado Ubaldo Fernandes, informou sobre uma audiência
-        que ocorrerá no dia 06 de junho para ouvir a opinião da população sobre o melhor
-        uso do terreno abandonado da Petrobrás. O deputado disponibilizou um ônibus
-        que passará pelas comunidades das Rocas, Praia do Meio, Santos Reis e Brasília
-        Teimosa para levar a população até a audiência.</p>
-        <p>Regularização fundiária na região de interesse social</p>
-        <p>• Luiz Antônio convidou todos para participar de um encontro no dia 15 de junho
-        para discutir a regularização fundiária na região de interesse social. O local será
-        definido posteriormente.</p>
-        <p>Realização de uma seresta para arrecadar fundos</p>
-        <p>• Francinete Venâncio propôs a realização de uma seresta para arrecadar fundos
-        para a associação. Foi sugerido que o evento seja realizado após as grandes
-        comemorações de São João. A facilidade de acesso ao centro social tornará mais
-        fácil a realização do evento.</p>
-        <p>Contribuição da associação para o Plano Plurianual Federal</p>
-        <p>• Decidiu-se que será realizada uma reunião separada para analisar os projetos da
-        associação e votar nos melhores para serem incluídos no Plano Plurianual
-        Federal. Os projetos selecionados pela comissão serão divulgados para aprovação
-        no PPA.</p>
-        <p>Informe sobre o curso de Surf Salva</p>
-        <p>• Balaio de Surf, informou sobre um curso gratuito de Surf Salva que ocorrerá na
-        Praia de Miami. Aqueles que tiverem interesse podem participar.</p>
-        <p>Assistência jurídica oferecida por Tia Mundinha</p>
-        <p>• Tia Mundinha informou que em sua residência é oferecida assistência jurídica
-        todas as quintas-feiras, das 16h às 20h, com o advogado Vitor Feitosa.</p>
-        <p>Evento no Pâmpano Esporte Club</p>
-        <p>• Tia Mundinha convidou todos da associação para participarem de um evento
-        organizado por ela no Pâmpano Esporte Club, no dia 12 de junho, das 16h às 20h.
-        O valor da entrada será de R$20,00 por mesa.</p>
-        <p>Projeto de hidroginástica na praia</p>
-        <p>• O professor Milton França compartilhou o projeto de hidroginástica na praia, que já
-        está sendo realizado há 25 anos. Ele mencionou as dificuldades em obter
-        visibilidade para o projeto, apesar da participação de pessoas influentes.</p>
-        <p>Encerramento com a oração do Pai Nosso</p>
-        <p>• A reunião foi encerrada com todos formando um grande círculo e segurando as
-        mãos para a oração do Pai Nosso.</p>
-      </article>
+    <main className={style.container}>
+      <section>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+      </section>
     </main>
   );
 };
---
+++ components/Post/index.tsx
@@ -0,0 +1,23 @@
+export function Post() {
+  return (
+    <article>
+      <header>
+        <h3>Ata da reunião do mês de maio</h3>
+        <p><time>30/05/2023</time> — Por Ana Paula</p>
+      </header>
+      <main>
+        <ul>
+          <li><p>Data: 30 de maio de 2023</p></li>
+          <li><p>Horário: 19:30 - 21:30</p></li>
+          <li><p>Local: Centro Social Nossa Senhora de Fátima na Praia do Meio</p></li>
+        </ul>
+        <p>
+          Tivemos um total de 22 membros presentes na reunião. Foi realizada a presentação do Rocas Divulgações, debatido a questão do novo Hospital Municipal de Natal, a audiência sobre o terreno abandonado da Petrobrás, a contribuição da associação para o Plano Plurianual Federal entre outras pautas importantes.
+        </p>
+      </main>
+      <footer>
+        <button>Abrir publicação completa</button>
+      </footer>
+    </article>
+  );
+};
